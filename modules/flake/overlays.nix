db133c7225c3c7dcce952a446e2d40bbd39b4518
wtroughton <winston@troughton.net>
2025-03-06 13:33:15 -0500

    Remove k3s overlay

--- modules/flake/overlays.nix
+++ modules/flake/overlays.nix
@@ -6,18 +6,18 @@
       inherit (inputs) globset;
     };
 
-    k3s = super.k3s_1_30.override {
-      buildGoModule = args: super.buildGoModule (args // super.lib.optionalAttrs (args.pname != "k3s-cni-plugins" && args.pname != "k3s-containerd") {
-        vendorHash = {
-          "sha256-HTUFv8WBIDiBQ860p3RiROF+kDzBekvgBAr2TJh036E=" = "sha256-Wzd0ief4HnMc/EW+AZOabTdl2yYJWohn51Unw1Nj850=";
-        }.${args.vendorHash};
-        # Source https://patch-diff.githubusercontent.com/raw/k3s-io/k3s/pull/9319.patch
-        # Remove when merged
-        patches = (args.patches or []) ++ [
-          ./patches/k3s-nix-snapshotter.patch
-        ];
-      });
-    };
+    # k3s = super.k3s_1_30.override {
+    #   buildGoModule = args: super.buildGoModule (args // super.lib.optionalAttrs (args.pname != "k3s-cni-plugins" && args.pname != "k3s-containerd") {
+    #     vendorHash = {
+    #       "sha256-HTUFv8WBIDiBQ860p3RiROF+kDzBekvgBAr2TJh036E=" = "sha256-Wzd0ief4HnMc/EW+AZOabTdl2yYJWohn51Unw1Nj850=";
+    #     }.${args.vendorHash};
+    #     # Source https://patch-diff.githubusercontent.com/raw/k3s-io/k3s/pull/9319.patch
+    #     # Remove when merged
+    #     patches = (args.patches or []) ++ [
+    #       ./patches/k3s-nix-snapshotter.patch
+    #     ];
+    #   });
+    # };
   };
 
   perSystem = { system, ... }: {
